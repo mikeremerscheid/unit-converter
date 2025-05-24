3a4ff5eac993a6f46c88c39e186b30af5116625f
mikeremerscheid <mikeremerscheid@gmail.com>
2025-05-24 16:06:09 -0400

    added converter from kg to lbs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,36 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule, FormsModule],
   template: `
     <div style="text-align:center; margin-top: 50px;">
-      <h1>Pound to Kilogram Converter</h1>
+      <h1>Unit Converter</h1>
 
-      <label for="pounds">Pounds:</label>
-      <input type="number" [(ngModel)]="pounds" id="pounds" />
+      <!-- Pounds to Kilograms -->
+      <div style="margin-bottom: 30px;">
+        <label for="pounds">Pounds:</label>
+        <input type="number" [(ngModel)]="pounds" id="pounds" />
+        <p>{{ pounds }} lbs = {{ poundsToKg(pounds) | number : '1.2-2' }} kg</p>
+      </div>
 
-      <p>{{ pounds }} lbs = {{ kilograms | number : '1.2-2' }} kg</p>
+      <!-- Kilograms to Pounds -->
+      <div>
+        <label for="kilograms">Kilograms:</label>
+        <input type="number" [(ngModel)]="kilograms" id="kilograms" />
+        <p>
+          {{ kilograms }} kg =
+          {{ kgToPounds(kilograms) | number : '1.2-2' }} lbs
+        </p>
+      </div>
     </div>
   `,
 })
 export class AppComponent {
   pounds: number = 0;
-  get kilograms(): number {
-    return this.pounds * 0.453592;
+  kilograms: number = 0;
+
+  poundsToKg(pounds: number): number {
+    return pounds * 0.453592;
+  }
+
+  kgToPounds(kg: number): number {
+    return kg / 0.453592;
   }
 }
